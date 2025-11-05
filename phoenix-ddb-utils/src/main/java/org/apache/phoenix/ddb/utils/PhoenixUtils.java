6de11af376af7c69a5d1fb3df2b47138771cb6d8
Viraj Jasani <vjasani@apache.org>
2025-11-05 04:45:01 -0800

    No explicit setting of REPLICATION_SCOPE

--- phoenix-ddb-utils/src/main/java/org/apache/phoenix/ddb/utils/PhoenixUtils.java
+++ phoenix-ddb-utils/src/main/java/org/apache/phoenix/ddb/utils/PhoenixUtils.java
@@ -142,8 +142,6 @@ public class PhoenixUtils {
      */
     public static String getTableOptions() {
         return " MERGE_ENABLED=false" + "," +
-                "REPLICATION_SCOPE=0" + "," +
-                "DISABLE_TABLE_SOR=true" + "," +
                 "IS_STRICT_TTL=false" + "," +
                 "UPDATE_CACHE_FREQUENCY=1800000" + "," +
                 "\"phoenix.max.lookback.age.seconds\"=97200" + "," +
